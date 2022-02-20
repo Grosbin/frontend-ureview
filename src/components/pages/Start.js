bf1adbf5d52d55d835038c95c428792bb2c52b4a
Grosbin <robertobetancourth69@gmail.com>
2022-02-19 23:42:08 -0600

    Corrección de texto

--- src/components/pages/Start.js
+++ src/components/pages/Start.js
@@ -4,7 +4,7 @@ export const Start = () => {
 	return (
 		<div className='main'>
 			<div className='container'>Aun falta programar esto : )</div>
-			<p>Tengo habilitado que puedan inicar sesion como docente y como estudiate.</p>
+			<p>Tengo habilitado que puedan iniciar sesión como docente y como estudiate.</p>
 			<p>Si quiren pueden entrar al apartado de Docente, esa parte es la que llevo mas avanzada.</p>
 		</div>
 	)
